88eb656e9cd7942427c7f5caf994dbc94175dbcb
Daniel Burrall <71298259+Dburrall@users.noreply.github.com>
2025-02-08 21:32:05 -0500

    Added Form for Crypto Masters

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<title>Crypto Masters</title>
+
+	<h1>Crypto Masters </h1>
+
+    <p>Enter the amount of crpyto</p>
+
+    <form>
+<label for="amount">Amount</label>
+<input id="amount">
+<label for="crypto">Crypto</label>
+<input>
+<select id="crypto">
+    <option>BTC</option>
+    <option>ETH</option>
+</select>
+<button type="submit">Convert</button>
+</form>
+
+</html>
